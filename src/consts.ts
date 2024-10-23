4ff03664aa3fa9882cba754e0fc39bd7ed78178e
Connor <connorsmith8@gmail.com>
2024-10-23 14:44:36 -0500

    move theme buttons to header and add about me

--- src/consts.ts
+++ src/consts.ts
@@ -9,7 +9,7 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION: "Landing page for my portfolio website.",
 };
 
 export const WORK: Metadata = {
